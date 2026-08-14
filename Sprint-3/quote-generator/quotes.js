04a5f9f2fb68e0839c5eb982fac634041880f07e
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-14 19:13:45 +0100

    find a random element from the array

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -1,6 +1,3 @@
-let gens = pickFromArray(quotes);
-document.querySelector("#quote").innerText = gens.quote;
-document.querySelector("#author").innerText = gens.author;
 // DO NOT EDIT BELOW HERE
 
 // pickFromArray is a function which will return one item, at
