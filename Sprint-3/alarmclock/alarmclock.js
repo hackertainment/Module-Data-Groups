8dc53fa015ae65eef12b20f2658310e8ef953d99
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-16 18:20:48 +0100

    Make the background flash color when the alarm clock finishes

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,20 +1,31 @@
+let timer = null;
+
 function showAlarm(second) {
   let minstr = Math.floor(second/60).toString().padStart(2, "0");
-  let secstr = (second%60).toString().padStart(2, "0");    
+  let secstr = (second%60).toString().padStart(2, "0");
   document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${minstr}:${secstr}`;
   if (second<=0) {
-    playAlarm();
     clearInterval(timer);
+    timer = null;
+    playAlarm();
+    document.querySelector("body").style.animation = "flash 0.5s cubic-bezier(1,0,0,1) 42";
+    setTimeout(() => {
+      document.querySelector("body").removeAttribute("style");
+    }, 20000);
   }
 }
 
 function setAlarm() {
   second = document.getElementById("alarmSet").value;
   showAlarm(second)
-  timer = setInterval(function () {
-    second--;
-    showAlarm(second);
-  }, 1000);
+  if (timer==null) {
+    timer = setInterval(() => {
+      if (second>0) {
+        second--;
+        showAlarm(second);
+      }
+    }, 1000);
+  }
 }
 
 // DO NOT EDIT BELOW HERE
