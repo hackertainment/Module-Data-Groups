20d20d307ad23b27dd64ca57751144377812e95b
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-16 16:31:32 +0100

    How the clock should work

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,21 @@
-function setAlarm() {}
+function showAlarm(second) {
+  let minstr = Math.floor(second/60).toString().padStart(2, "0");
+  let secstr = (second%60).toString().padStart(2, "0");    
+  document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${minstr}:${secstr}`;
+  if (second<=0) {
+    playAlarm();
+    clearInterval(timer);
+  }
+}
+
+function setAlarm() {
+  second = document.getElementById("alarmSet").value;
+  showAlarm(second)
+  timer = setInterval(function () {
+    second--;
+    showAlarm(second);
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
